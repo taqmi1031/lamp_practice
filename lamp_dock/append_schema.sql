cff0d065210062b02d244831997b25e7f101cc95
Takumi Nagaya <taqmi.na1031@icloud.com>
2020-05-25 08:32:57 +0900

    テーブル修正

--- lamp_dock/append_schema.sql
+++ lamp_dock/append_schema.sql
@@ -12,9 +12,7 @@ SET time_zone = "+00:00";
 CREATE TABLE 'history' (
     'order_id' int(11) NOT NULL,
     'user_id' int(11) NOT NULL,
-    'item_id' int(11) NOT NULL,
-    'cart_id' int(11) NOT NULL,
-    'created' datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    'created' datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -25,9 +23,11 @@ CREATE TABLE 'history' (
 --
 
 CREATE TABLE 'details' (
-    'detail_id' int(11) NOT NULL,    
     'order_id' int(11) NOT NULL,
-    'created' datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    'item_id' int(11) NOT NULL,
+    'price' int(11) NOT NULL, -- 価格変更機能を持たせるため
+    'amount' int(11) NOT NULL,
+    'created' datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -38,17 +38,15 @@ CREATE TABLE 'details' (
 -- テーブルのインデックス `history`
 --
 ALTER TABLE `history`
-  ADD PRIMARY KEY (`order_id`);
+  ADD PRIMARY KEY (`order_id`),
+  ADD KEY `user_id` (`user_id`);
 
 --
 -- テーブルのインデックス `details`
 --
 ALTER TABLE `details`
-  ADD PRIMARY KEY (`detail_id`),
-  ADD KEY `order_id` (`order_id`),
+  ADD PRIMARY KEY `order_id` (`order_id`),
   ADD KEY `item_id` (`item_id`),
-  ADD KEY `user_id` (`user_id`),
-  ADD KEY `cart_id` (`cart_id`);
 
 --
 -- ダンプしたテーブルのAUTO_INCREMENT
@@ -64,7 +62,7 @@ ALTER TABLE `history`
 -- テーブルのAUTO_INCREMENT `details`
 --
 ALTER TABLE `details`
-  MODIFY `detail_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- ダンプしたテーブルの制約
@@ -74,13 +72,11 @@ ALTER TABLE `details`
 -- テーブルの制約 `history`
 --
 ALTER TABLE `history`
-  ADD CONSTRAINT `history_ibfk_1` FOREIGN KEY (`detail_id`) REFERENCES `details` (`detail_id`);
+  ADD CONSTRAINT `history_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
 --
 -- テーブルの制約 `details`
 --
 ALTER TABLE `details`
-  ADD CONSTRAINT `details_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `history` (`order_id`);
-  ADD CONSTRAINT `details_ibfk_2` FOREIGN KEY (`item_id`) REFERENCES `items` (`item_id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `details_ibfk_3` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
-  ADD CONSTRAINT `details_ibfk_4` FOREIGN KEY (`cart_id`) REFERENCES `cart` (`cart_id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `details_ibfk_1` FOREIGN KEY (`item_id`) REFERENCES `items` (`item_id`) ON DELETE CASCADE;
 COMMIT;
